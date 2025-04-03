286349103f43cf101e30733be7525bf10d90e404
hoda <julpietrzak147@gmail.com>
2025-04-03 11:11:49 +0200

    :D

--- lab3/MainWindow.axaml.cs
+++ lab3/MainWindow.axaml.cs
@@ -2,16 +2,20 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using ReactiveUI;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using ReactiveUI;
+
 
 namespace lab3;
 
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
     public ObservableCollection<pracownik> pracownicy { get; } = new ObservableCollection<pracownik>();
+    
 
     public MainWindow() {
         InitializeComponent();
@@ -32,7 +36,14 @@ public partial class MainWindow : Window, INotifyPropertyChanged
             if (button.Content.ToString() == "Dodaj") {
                 dod = new dodawanie(this);
                 dod.Show();
-                
+                BuyMusicCommand = ReactiveCommand.CreateFromTask(async () => {
+                    dod = new dodawanie(this);
+                    var result = await ShowDialog.Handle(dod);
+                    if (result != null)
+                    {
+                        pracownicy.Add(result);
+                    }
+                });
             }
             if (button.Content.ToString() == "Usuń") {
                 
